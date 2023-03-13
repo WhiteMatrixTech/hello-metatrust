f94c78909e86c38d093b45646f7e37e9d9904906
caoxm <386707656@qq.com>
2023-03-13 12:01:47 +0800

    add all common Hacks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -75,7 +75,7 @@ const config: HardhatUserConfig = {
                 version: "0.8.17",
             },
             {
-                version: "0.4.17",
+                version: "0.7.6",
             },
         ],
     },
