67c01916b3da3f93993bfd2a1d153f10ce9c7a2c
caoxm <386707656@qq.com>
2023-03-13 16:30:03 +0800

    add theri

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -73,6 +73,12 @@ const config: HardhatUserConfig = {
         compilers: [
             {
                 version: "0.8.17",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
             },
             {
                 version: "0.7.6",
